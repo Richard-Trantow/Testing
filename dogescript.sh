13664db9725acd389f9ff3e4dae8b5b2dd519cec
Richard Trantow <trant025@umn.edu>
2016-07-22 17:04:16 -0500

    Cleaned up script1.sh and renamed to dogescript.sh

--- dogescript.sh
+++ dogescript.sh
@@ -10,25 +10,22 @@ echo
 echo "I'm setting two variables now."
 Var1="3"
 Var2="9"
-echo "Variable one is:$Var1."
-echo "And this is a number:$Var2."
+echo "Variable one is: $Var1."
+echo "And this is a number: $Var2."
 echo "The product of these two numbers is 27."
 echo "27 is also achieved by raising 3 to the power of 3."
 echo "There are 3 sides to a triangle."
-ehco "Therefore, we can conclude that anyone of the age of 27 is also a member of the /n ILLUMINATI."
+echo "Therefore, we can conclude that anyone 27 and older is also a member of the /n ILLUMINATI."
+echo
 echo "Such secret.."
 echo "Many Illuminati..."
 echo "Wow."
-
 echo "And now, you may have your command line back."
 echo
-
 echo "HA. I lied. I'm going to list the contents of your GitHub folder."
 echo
-
 echo "Here you are!"
 echo
-ls ~/GitHub
 echo
 echo "Your secrets are not safe, mwhuahahaha!" 
 echo
