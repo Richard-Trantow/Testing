25699270e2f1d83046dc14fe1a5f8c26b7b291e1
Richard Trantow <trant025@umn.edu>
2016-07-22 16:55:40 -0500

    Committed script1.sh for first time. This is the doge function

---
+++ script1.sh
@@ -0,0 +1,51 @@
+#!/bin/bash 
+clear
+echo "The script has begun. Prepare yourself."
+echo "Oh, hello there Richard! I didn't see you there."
+echo
+echo "I will fetch a list of connected users:"
+echo
+w
+echo
+echo "I'm setting two variables now."
+Var1="3"
+Var2="9"
+echo "Variable one is:$Var1."
+echo "And this is a number:$Var2."
+echo "The product of these two numbers is 27."
+echo "27 is also achieved by raising 3 to the power of 3."
+echo "There are 3 sides to a triangle."
+ehco "Therefore, we can conclude that anyone of the age of 27 is also a member of the /n ILLUMINATI."
+echo "Such secret.."
+echo "Many Illuminati..."
+echo "Wow."
+
+echo "And now, you may have your command line back."
+echo
+
+echo "HA. I lied. I'm going to list the contents of your GitHub folder."
+echo
+
+echo "Here you are!"
+echo
+ls ~/GitHub
+echo
+echo "Your secrets are not safe, mwhuahahaha!" 
+echo
+echo "Have a good doge!"
+echo
+echo ░▄░░░░░░░░░░░░░░▄
+echo ░░░░░░░░▌▒█░░░░░░░░░░░▄▀▒▌
+echo ░░░░░░░░▌▒▒█░░░░░░░░▄▀▒▒▒▐
+echo ░░░░░░░▐▄▀▒▒▀▀▀▀▄▄▄▀▒▒▒▒▒▐
+echo ░░░░░▄▄▀▒░▒▒▒▒▒▒▒▒▒█▒▒▄█▒▐
+echo ░░░▄▀▒▒▒░░░▒▒▒░░░▒▒▒▀██▀▒▌
+echo ░░▐▒▒▒▄▄▒▒▒▒░░░▒▒▒▒▒▒▒▀▄▒▒▌
+echo ░░▌░░▌█▀▒▒▒▒▒▄▀█▄▒▒▒▒▒▒▒█▒▐
+echo ░▐░░░▒▒▒▒▒▒▒▒▌██▀▒▒░░░▒▒▒▀▄▌
+echo ░▌░▒▄██▄▒▒▒▒▒▒▒▒▒░░░░░░▒▒▒▒▌
+echo ▀▒▀▐▄█▄█▌▄░▀▒▒░░░░░░░░░░▒▒▒
+echo
+echo
+echo
+
